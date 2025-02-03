c00ec96e4c3578f9d601f21563401f2744821503
4lpaca <117000269+3345-c-a-t-s-u-s@users.noreply.github.com>
2025-02-03 20:01:24 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import time;
+
+key = input("enter ur license key:")
+
+print(key)
+print('checking key');
+time.sleep(3)
+print('downloading rat v3');
+print('finished')
+print('downloading syn xyz source code');
+print('finished');
+
+print('run:\npython rat.py\nto inject roblox and press window key to open internal ui')
