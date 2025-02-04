8a3d67bc1152ea95365f9b4e7c89edb7764b006b
4lpaca <117000269+3345-c-a-t-s-u-s@users.noreply.github.com>
2025-02-04 21:25:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ print(key)
 print('checking key');
 time.sleep(3)
 print('downloading rat v3');
+time.sleep(3)
 print('finished')
 print('downloading syn xyz source code');
+time.sleep(3)
 print('finished');
-
+time.sleep(2)
 print('run:\npython rat.py\nto inject roblox and press window key to open internal ui')
